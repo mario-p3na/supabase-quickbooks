9200c7430ac18c07f06f1c973169dc8715710936
Mario Pena <mpena007@gmail.com>
2024-01-25 15:52:38 -0800

    chore: print event obj

--- tasks/createUpsertInvoiceTask.ts
+++ tasks/createUpsertInvoiceTask.ts
@@ -1,3 +1,3 @@
 export const createUpdateInvoiceTask = async (payload: any) => {
-  console.log(`Processing payload: ${payload}`);
+  console.log(`Processing payload: ${JSON.stringify(payload)}`);
 };
