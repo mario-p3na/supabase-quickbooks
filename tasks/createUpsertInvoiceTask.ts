2b84f00583e67099f6a36e9bd3f1285e3f283f30
Mario Pena <mpena007@gmail.com>
2024-01-25 16:19:15 -0800

    chore: debug

--- tasks/createUpsertInvoiceTask.ts
+++ tasks/createUpsertInvoiceTask.ts
@@ -1,3 +1,9 @@
 export const createUpdateInvoiceTask = async (payload: any) => {
-  console.log(`Processing payload: ${JSON.stringify(payload)}`);
+  const p = JSON.stringify(payload);
+  console.log(`Processing payload`);
+  console.log(JSON.stringify(payload));
+  console.log(
+    payload["eventNotifications"][0]["dataChangeEvent"]["entities"][0]
+  );
+  console.log(p);
 };
