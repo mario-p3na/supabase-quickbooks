d0ddd4e82801973a4e98abee695c0fb283c8d225
Mario Pena <mpena007@gmail.com>
2024-01-28 13:00:05 -0800

    fix: wrap task in try-catch

--- tasks/createUpsertInvoiceTask.ts
+++ tasks/createUpsertInvoiceTask.ts
@@ -7,33 +7,37 @@ const QuickBooks = require("node-quickbooks");
 dotenv.config();
 
 export const createUpdateInvoiceTask = async (payload: any) => {
-  if (payload) {
-    const qbo = new QuickBooks(
-      process.env.QUICKBOOKS_CLIENT_ID,
-      process.env.QUICKBOOKS_CLIENT_SECRET,
-      process.env.QUICKBOOKS_OAUTH_TOKEN,
-      false,
-      process.env.QUICKBOOKS_REALM_ID,
-      true,
-      true,
-      null,
-      "2.0",
-      process.env.QUICKBOOKS_REFRESH_TOKEN
-    );
+  try {
+    if (payload) {
+      const qbo = new QuickBooks(
+        process.env.QUICKBOOKS_CLIENT_ID,
+        process.env.QUICKBOOKS_CLIENT_SECRET,
+        process.env.QUICKBOOKS_OAUTH_TOKEN,
+        false,
+        process.env.QUICKBOOKS_REALM_ID,
+        true,
+        true,
+        null,
+        "2.0",
+        process.env.QUICKBOOKS_REFRESH_TOKEN
+      );
 
-    payload.eventNotifications.map((eventNotification: any) => {
-      eventNotification.dataChangeEvent.entities.map((entity: any) => {
-        if (entity.operation === "Update" && entity.name === "Invoice") {
-          // Get the invoice from QuickBooks
-          qbo.getInvoice(entity.id, (err: any, invoice: any) => {
-            if (err) {
-              console.log(err);
-            } else {
-              upsertInvoice(invoice);
-            }
-          });
-        }
+      payload.eventNotifications.map((eventNotification: any) => {
+        eventNotification.dataChangeEvent.entities.map((entity: any) => {
+          if (entity.operation === "Update" && entity.name === "Invoice") {
+            // Get the invoice from QuickBooks
+            qbo.getInvoice(entity.id, (err: any, invoice: any) => {
+              if (err) {
+                console.log(err);
+              } else {
+                upsertInvoice(invoice);
+              }
+            });
+          }
+        });
       });
-    });
+    }
+  } catch (err) {
+    console.log(`Error while attempting to update record: ${err}`);
   }
 };
